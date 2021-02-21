e895c261e01d4ec0722268136b8a562529faf74c
Suriya <suriya.prakasam@gmail.com>
2021-02-21 12:51:04 +0000

    Refactor RabbitBus MQ with Parameterless constructure

--- MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit.Transfer.Api/Startup.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.eventHandlers;
+using MicroRabbit.Transfer.Domain.Events;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MicroRabbit.Transfer.Api
 {
@@ -25,7 +33,26 @@ namespace MicroRabbit.Transfer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TransferDbContext>(Options =>
+            {
+                Options.UseSqlServer(Configuration.GetConnectionString("TransferDbConnection"));
+            }
+                );                  
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V2", new Info { Title = "Transfer MicroService", Version = "V2" });
+            }
+                );
+
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,8 +68,22 @@ namespace MicroRabbit.Transfer.Api
                 app.UseHsts();
             }
 
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/Swagger/V2/Swagger.json", "V2");
+            }
+            );
             app.UseHttpsRedirection();
             app.UseMvc();
+            ConfigureEventBus(app);
+        }
+
+        private  void ConfigureEventBus(IApplicationBuilder app)
+        {
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            eventBus.subscribe<TransferCreatedEvent, TransferEventHandler>();
         }
     }
 }
---
+++ MicroRabbit.Transfer.Domain/Interfaces/ITransferRepository.cs
@@ -0,0 +1,11 @@
+﻿using MicroRabbit.Transfer.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Transfer.Domain.Interfaces
+{
+    public interface ITransferRepository
+    {
+        void Add(TransferLog transferLog);
+        IEnumerable<TransferLog> GetTransferLogs();
+    }
+}
